e83497890d3f859e8f622427cbce9dccce9f8c32
Sonu Tudu <85930927+sonu-12871@users.noreply.github.com>
2022-07-09 13:28:57 +0530

    Add files via upload

---
+++ src/main/java/com/example/FirstGroupProject/FirstGroupProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.FirstGroupProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstGroupProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstGroupProjectApplication.class, args);
+	}
+
+}
